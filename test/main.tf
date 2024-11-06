0eb9e8820e2d65b795fc989593b9a2d728ff104c
Erin Pronobis <erinpronobis@Erins-MacBook-Air.local>
2024-11-06 07:32:29 -0500

    updated s3 bucket policy

--- test/main.tf
+++ test/main.tf
@@ -36,28 +36,35 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+/*
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.my-cdn-cf-policy.json
 }
+*/
 
-data "aws_iam_policy_document" "my-cdn-cf-policy" {
-  statement {
-    sid = "1"
-    principals {
-      type        = "AWS"
-      identifiers = [aws_cloudfront_origin_access_identity.my_oai.iam_arn]
-    }
-
-    actions = [
-      "s3:GetObject"
+resource "aws_s3_bucket_policy" "cloudfront_s3_bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Id      = "PolicyForCloudFrontPrivateContent"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
     ]
-
-    resources = [
-      aws_s3_bucket.bucket.arn,
-      "${aws_s3_bucket.bucket.arn}/*",
-    ]
-  }
+  })
 }
 ################################################################################
 # Cloudfront Distribution
